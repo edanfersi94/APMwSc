34a0cad718065034fb3b97778a482d6f07e748da
Edward Fernandez <edwardfernandez26@gmail.com>
2015-05-05 22:34:27 -0430

    Se configuro la base de datos local.

--- data/model.py
+++ data/model.py
@@ -1,37 +1,30 @@
-"""
-	UNIVERSIDAD SIMON BOLIVAR
-	Departamento de Computacion y Tecnologia de la Informacion.
-	Ingeniería de Software I (CI-3715)
-	Abril - Julio 2015
-
-	AUTORES:
-		Nicolas
-		Edward Fernandez.   Carnet: 10-11121
-	
-
-	DESCRIPCION: 
-
-	Versión 0.1 05-05-2015
-
-"""
 # Librerias a utilizar.
-from flask import Flask, request
-from flask.ext.sqlalchemy import SQLAlchemy
-from app import db
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine.url import URL
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+import settings
+
+db = declarative_base()
 
 #-------------------------------------------------------------------------------
 # Tablas de la base de datos a definir.
 
-# Tabla Usuario.
-class User(db.Model):
-	__tablename__ = 'user'
-	fullname = db.Column(db.String(50), unique = True)
-	username = db.Column(db.String(16), primary_key = True)
-	password = db.Column(db.String(16), unique = True)
-	email = db.Column(db.String(30), unique = True)
-	iddpt = db.Column(db.Integer, db.ForeignKey('dpt.iddpt'), nullable = False)
-	idrole = db.Column(db.Integer, db.ForeignKey('role.idrole'), unique = True)
 
+# Tabla Usuario.
+class User(db):
+	__tablename__ = 'user'
+	fullname = Column(String(50), unique = True)
+	username = Column(String(16), primary_key = True)
+	password = Column(String(16), unique = True)
+	email = Column(String(30), unique = True)
+	iddpt = Column(Integer, ForeignKey('dpt.iddpt'), nullable = False)
+	idrole = Column(Integer, ForeignKey('role.idrole'), unique = True)
+	
 	def __init__(self,fullname, username, password, email, iddpt, idrole):
 		self.fullname = fullname
 		self.username = username
@@ -39,29 +32,40 @@ class User(db.Model):
 		self.email = email
 		self.iddpt = iddpt
 		self.idrole = idrole
+	
+	def __rep__(self):
+		return "<User('%s','%s','%s','%s','%d','%d' )>" % (self.fullname,self.username,self.password,self.email,self.iddpt,self.idrole)
+
 
 # Tabla Departamento.
-class Dpt(db.Model):
+class Dpt(db):
 	__tablename__ = 'dpt'
-	iddpt = db.Column(db.Integer, primary_key = True)
-	namedpt = db.Column(db.String(50), unique = True)
-	users = db.relationship('User', backref = 'dpt')
+	iddpt = Column(Integer, primary_key = True)
+	namedpt = Column(String(50), unique = True)
+	users = relationship('User', backref = 'dpt')
 
 	def __init__(self, iddpt, namedpt, users):
 		self.iddpt = iddpt
 		self.namedpt = namedpt
 		self.users = users
+		
+	def __rep__(self):
+		return "<DPT('%d', '%s')>" % (self.iddpt,self.namedpt)
 
 # Tabla Role.
-class Role(db.Model):
+class Role(db):
 	__tablename__ = 'role'
-	idrole = db.Column(db.Integer, primary_key = True)
-	namerole = db.Column(db.String(50), unique = True)
-	users = db.relationship('User', backref = 'role')
+	idrole = Column(Integer, primary_key = True)
+	namerole = Column(String(50), unique = True)
+	users = relationship('User', backref = 'role')
 
-	def __init__(self, iddpt, namedpt, users):
+	def __init__(self, idrole, namerole):
 		self.idrole = idrole
 		self.namerole = namerole
-		self.users = users
-
+		
+	def __rep__(self):
+		return "<ROLE('%d', '%s')>" % (self.idrole,self.namerole)
 #-------------------------------------------------------------------------------
+
+engine = create_engine(URL(**settings.DATABASE))	
+db.metadata.create_all(engine)
---
+++ data/settings.py
@@ -0,0 +1,11 @@
+
+# Definicion de la base de datos.
+DATABASE = {
+	'drivername': 'postgres',
+	'host': 'localhost',
+	'port': '5432',
+	'username': 'BMO',
+	'password': '',
+	'database': 'prueba'
+}
+

92a4962538dba14f66b5d03aa5784f3f461d2f0e
Edward Fernandez <edwardfernandez26@gmail.com>
2015-05-06 01:07:28 -0430

    Se añadieron busqueda y eliminacion.

--- business/access-control/role.py
+++ business/access-control/role.py
@@ -9,10 +9,10 @@
 		Nicolas
 
     DESCRIPCION: 
-		
+	
 """
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
 
 # Librerias a utilizar.
 
@@ -26,27 +26,121 @@ import model
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------
 
 # Se realiza la conexion con la bases de datos para realizar cambios en ella.
 DBSession = sessionmaker(bind=model.engine)
 session = DBSession()
 
+# Clase que tendra las diferentes funcionalidades de la tabla "Role".
 class clsRole():
 
+	#-------------------------------------------------------------------------------
+	
 	def insert_role(self, newIdRole, newNameRole):
+		"""
+			@brief Funcion que permite insertar un nuevo rol en la base de datos.
+			
+			@param newIdRole: Identificador del rol a insertar.
+			@param newNameRole: Nombre del rol a insertar.
+			
+			@return void.
+		"""
+		
 		newRole = model.Role(newIdRole, newNameRole)
 		session.add(newRole)
 		session.commit()
+		
+	#-------------------------------------------------------------------------------
+	
+	def find_IdRole(self, idRole):
+		"""
+			@brief Funcion que realiza la busqueda del rol cuyo identificador
+				   sea "idRole".
+			
+			@param idRole: Identificador del rol a buscar.
+			
+			@return subquery con la consulta solicitada.
+		"""
+		
+		result = session.query(model.Role).filter(model.Role.idrole==idRole).all()
+		return(result)
+	
+	#-------------------------------------------------------------------------------
+	def find_NameRole(self, nameRole):
+		"""
+			@brief Funcion que realiza la busqueda de los roles cuyo identificador
+				   sea "nameRole".
+			@return subquery con la consulta solicitada.
+		"""
+		result = session.query(model.Role).filter(model.Role.namerole==nameRole).all()
+		return(result)
+		
+	#-------------------------------------------------------------------------------
+	
+	def find_listIdRole(self):
+		"""
+			@brief Funcion que devuelve los identificadores de los diferentes roles
+				   que se encuentran almacenados en la base de datos.
+			
+			@param no admite parametros.
+			
+			@return subquery con la consulta solicitada.
+		"""
+		result = session.query(model.Role.idrole.label('id')).all()
+		return(result)
+		
+	#-------------------------------------------------------------------------------
+	def find_listNameRole(self):
+		"""
+			@brief Funcion que devuelve los nombres de los diferentes roles que se
+				   encuentran almacenados en la base de datos.
+			
+			@param no admite parametros.
+			
+			@return subquery con la consulta solicitada.
+		"""
+		result = session.query(model.Role.namerole.label('name')).all()
+		return(result)
+	
+	#-------------------------------------------------------------------------------
+	
+	def delete_role(self, roleId):
+		"""
+			@brief Funcion que elimina el rol cuyo identificador sea "roleId".
+			
+			@param roleId: Identificador del rol a eliminar.
+			
+			@return void.
+		"""
+		session.query(model.Role).filter(model.Role.idrole==roleId).delete()
+		session.commit()
 
-
+	#-------------------------------------------------------------------------------
 
 """
-Prueba.
-		
 role1 = clsRole()
-role1.insert_roles(1,'departamento1')
+# PRUEBA: Insertar.
+
 role1.insert_roles(2,'departamento2')
 role1.insert_roles(3,'departamento3')
 
+
+#role1.insert_role(1,'departamento1')
+result = role1.find_IdRole(1)
+for v in result:
+	print(v.namerole)
+result = role1.find_NameRole("departamento3")
+for v in result:
+	print(v.idrole)
+result = role1.find_listNameRole()
+for v in result:
+	print(v.name_label)
+result = role1.find_listIdRole()
+for v in result:
+	print(v.id_label)
+# PRUEBA: Eliminar
+#result = role1.find_IdRole(1)
+#role1.delete_role(1)
+#role1.rolling_back_role()
 """

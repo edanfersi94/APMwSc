71d425b8ca0daa1d5ae655eab3e4227b97b95af8
Edward Fernandez <edwardfernandez26@gmail.com>
2015-05-06 01:06:43 -0430

    Se añadio el membrete

--- data/settings.py
+++ data/settings.py
@@ -1,3 +1,17 @@
+"""
+    UNIVERSIDAD SIMON BOLIVAR
+    Departamento de Computacion y Tecnologia de la Informacion.
+    CI-3715 - Ingenieria de Software I (CI-3715)
+    Abril - Julio 2015
+
+    AUTORES:
+        Edward Fernandez.   Carnet: 10-11121
+		Nicolas
+
+    DESCRIPCION: Script que contiene la lista de variables globales que permitiran
+				 establecer la conexion con la bases de datos.
+	
+"""
 
 # Definicion de la base de datos.
 DATABASE = {

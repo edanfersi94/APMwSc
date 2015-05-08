b01f4c58b8c328be8df2930b2db5befd30a9a574
Edward Fernandez <edwardfernandez26@gmail.com>
2015-05-07 23:59:01 -0430

    Se agregaron 21 casos de pruebas

--- testing/access-control/testrole.py
+++ testing/access-control/testrole.py
@@ -20,10 +20,11 @@
 import os
 import sys
 
-# PATH que permite utilizar al modulo "model.py"
+# PATH que permite utilizar al modulo "role.py"
 sys.path.append('../../business/access-control')
 from role import clsRole, session
 
+# PATH que permite utilizar al modulo "model.py"
 sys.path.append('../../data')
 import model
 
